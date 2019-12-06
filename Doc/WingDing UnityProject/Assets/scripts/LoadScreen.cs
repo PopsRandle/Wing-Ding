e49bc1ac0244fc426f51f613184ec8949bd1c658
PopsRandle <55261821+PopsRandle@users.noreply.github.com>
2019-12-05 18:18:03 -0600

    Fixes

---
+++ Doc/WingDing UnityProject/Assets/scripts/LoadScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void LoadGame()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Game Screen", LoadSceneMode.Additive);
+    }
+
+}     
